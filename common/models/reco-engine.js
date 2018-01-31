4924de028947f2291631866de354e85099088544
trilaiclub <trilaiclub@gmail.com>
2018-01-31 12:36:33 +0530

    final changes

--- common/models/reco-engine.js
+++ common/models/reco-engine.js
@@ -15,6 +15,8 @@ module.exports = function(Recoengine) {
 
       Recoengine.sClient = solr.createClient(solrBucket.client);
 
+      Recoengine.articles = [];
+
       Recoengine.sqActivitiesByUserID = function(squery, userID, cb) {
 
           var response = "";
@@ -109,19 +111,33 @@ module.exports = function(Recoengine) {
     //Find in rules
 
         Recoengine.formatRulesAsCategory = function(rules) {
-            var result = {};
-            for(var index = 0; index < rules.length; index++){
 
-                var category = rules[index].category;
+                    var result = {}; result.high = {}; result.low = {};
+                    var high = {};
+                    var low = {};
 
-                if(!(category in result))
-                  result[category] = [];
+                    for(var index = 0; index < rules.length; index++){
 
-                result[category].push(rules[index]);
-            }
+                        var category = rules[index].category;
 
-            return result;
-        }
+                        if(category == 'All') {
+
+                          if(!(category in high))
+                            high[category] = [];
+
+                          high[category].push(rules[index]);
+                        } else {
+
+                          if(!(category in low))
+                            low[category] = [];
+
+                          low[category].push(rules[index]);
+                        }
+                    }
+
+                    result = Object.assign(high, low);
+                    return result;
+                }
 
     //Train
 
@@ -200,7 +216,6 @@ module.exports = function(Recoengine) {
 
           var purchasedQuery = config.uir.compute_purchased.params;
           var action = "purchased";
-          var pResponse = {};
 
           console.log("Given " + action + " Query: " + purchasedQuery); //debug
 
@@ -211,44 +226,47 @@ module.exports = function(Recoengine) {
               Recoengine.find({where: {action:action}}, function(err, rRules){
 
                   var ruleSet = Recoengine.formatRulesAsCategory(rRules);
-                  var allRule = ("All" in ruleSet);
+                  var isAllAvailable = ("All" in ruleSet);
 
                   console.log(ruleSet); //debug
 
-                  /*request(options, function (error, response, body) {
-
-                     if(error)
-                        pResponse = error;
-                     else
-                        pResponse = Recoengine.parseBody(body);
-
-                        cb(null, pResponse);
-                  });*/
-
-                  var proxyJson = process.cwd() + '/' + "proxysample.json";
-                  var pResponse = require(proxyJson);
-
+                  var counter = Object.keys(uiRanks.searchkeys).length;
                   for (const [key, value] of Object.entries(uiRanks.searchkeys)) {
 
+                      --counter;
                       var keyPair = key.split('~');
                       var category = keyPair[0];
                       var phrase = category + ' ' + keyPair[1];
 
-                      var header = (category in ruleSet) ? category : "All";
+                      var headers = [];
+                      if(isAllAvailable) headers.push('All');
+                      if(ruleSet[category]) headers.push(category);
+                      console.log(headers);
 
-                      if(header in ruleSet) {
+                      var hCounter = headers.length;
+                      for(var h = 0; h < headers.length; h++) {
 
-                          for(var index = 0; index < ruleSet[header].length; index++) {
+                          --hCounter;
+                          var header = headers[h];
 
+                          if(header in ruleSet) {
+                              var rCounter = ruleSet[header].length;
+                              for(var index = 0; index < ruleSet[header].length; index++) {
+                                  console.log(header);
                                   var options = Recoengine.process(phrase, ruleSet[header][index]);
                                   console.log(options); //debug
+
+                                  --rCounter;
+                                  request(options, Recoengine.requestProxy(counter, hCounter, rCounter, cb));
+
+                              }
                           }
                       }
                   }
 
               });
 
-              cb(null, uiRanks);
+              //cb(null, uiRanks);
             });
           });
 
@@ -261,4 +279,73 @@ module.exports = function(Recoengine) {
               accepts: {arg: 'userid', type: 'string'},
               returns: {arg: 'response', type: 'object', 'http': {source: 'res'}}
         });
+
+        Recoengine.requestProxy = function(counter, hCounter, rCounter, cb) {
+
+            return function (error, response, body) {
+
+                var pResponse = {};
+
+                if(error)
+                   pResponse = error;
+                else
+                   pResponse = Recoengine.parseBody(body);
+
+                console.log(pResponse);
+                 //mocked data to avoid exploidation of 250 limit in feedly
+                 //var proxyJson = process.cwd() + '/' + "proxysample.json";
+                 //var pResponse = require(proxyJson);
+
+                 //console.log(pResponse);
+                 var eContents = pResponse;
+                 let iCounter = eContents.items.length;
+                 if(iCounter > 0) {
+                     for(var iIndex = 0; iIndex < eContents.items.length; iIndex++ ) {
+
+                       --iCounter;
+                       Recoengine.addArticle(eContents.items[iIndex]);
+
+                       var status = (counter + hCounter + rCounter + iCounter);
+                       console.log("---"+counter+"----"+hCounter+"---"+rCounter+"-----"+iCounter+"|" + status);
+
+                       if(status == 0 ) cb(null, Recoengine.articles);
+                     }
+                 } else {
+
+                       var status = (counter + hCounter + rCounter + iCounter);
+                       console.log("---"+counter+"----"+hCounter+"---"+rCounter+"-----"+iCounter+"|" + status);
+
+                       if(status == 0 ) cb(null, Recoengine.articles);
+                 }
+
+                   //cb(null, pResponse);
+             }
+        }
+
+
+        Recoengine.addArticle = function(obj) {
+
+                    var imgUrl = '';
+
+                    if('thumbnail' in obj)
+                        imgUrl = (obj.thumbnail.length > 0) ? obj.thumbnail[0].url : null;
+
+                    var content = 'Nothing available';
+
+                    if('summary' in obj)
+                    if('content' in obj.summary)
+                      content = obj.summary.content;
+
+                    var result = {
+                                     "id": obj.id,
+                                     "title": obj.title,
+                                     "description": content,
+                                     "sourceurl": obj.canonicalUrl,
+                                     "urltoimage": imgUrl
+                                   };
+
+                    console.log(result); //debug
+
+                    Recoengine.articles.push(result);
+                }
 };
